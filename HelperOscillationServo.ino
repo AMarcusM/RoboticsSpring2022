dfadbeb6afe2dafc886bcf408b8058cd63ad5b86
AMarcusM <73672594+AMarcusM@users.noreply.github.com>
2021-11-05 16:44:01 -0400

    Create HelperOscillationServo.ino

---
+++ HelperOscillationServo.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo servo;
+int angle = 10;
+int switchstate = 1;
+void setup() {
+servo.attach(8);
+servo.write(angle);
+}
+
+void loop()
+{
+if (switchstate = 1);
+// scan from 0 to 180 degrees
+for(angle = 10; angle < 180; angle++)
+{
+servo.write(angle);
+delay(15);
+}
+// now scan back from 180 to 0 degrees
+for(angle = 180; angle > 10; angle–)
+{
+servo.write(angle);
+delay(15);
+}
+}
